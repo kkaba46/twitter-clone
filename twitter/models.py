7476a5af4101503b81609f0066614a456c3443a4
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2014-02-03 01:46:27 +0200

    DB and Model classes are updated

--- twitter/models.py
+++ twitter/models.py
@@ -7,19 +7,26 @@ class Kullanici(models.Model):
 	password = models.CharField(max_length=100)
 	name = models.CharField(max_length=100)
 	photo = models.FileField(upload_to="/Users/emrahayanoglu/projects/twitterClone/upload",null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 class Twit(models.Model):
 	kullanici = models.ForeignKey(Kullanici)
 	ebeveyn = models.ForeignKey('self', null=True)
 	mesaj = models.CharField(max_length=140)
 	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 	is_deleted = models.BooleanField()
 
 class DM(models.Model):
 	mesaj = models.CharField(max_length=250)
 	kime = models.ForeignKey(Kullanici, related_name="kime")
 	kimden = models.ForeignKey(Kullanici, related_name="kimden")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 class Takip(models.Model):
-	eden = models.ManyToManyField(Kullanici, related_name="eden")
-	edilen = models.ManyToManyField(Kullanici, related_name="edilen")
+	eden = models.ForeignKey(Kullanici, related_name="eden")
+	edilen = models.ForeignKey(Kullanici, related_name="edilen")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
